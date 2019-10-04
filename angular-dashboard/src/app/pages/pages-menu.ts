485bb9cce3b56343f4a67d1cf8f53f639060bb8d
Antonio Cecere <antocecere77@gmail.com>
2019-10-04 16:03:11 +0200

    Angular first commit

---
+++ angular-dashboard/src/app/pages/pages-menu.ts
@@ -0,0 +1,50 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Dashboard',
+    icon: 'home-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  },
+  {
+    title: 'Check',
+    icon: 'lock-outline',
+    children: [
+      {
+        title: 'Catalogue',
+        link: '/check/catalogue'
+      },
+      {
+        title: 'Inventory',
+        link: '/check/inventory'
+      }
+    ]
+  },
+  {
+    title: 'Auth',
+    icon: 'lock-outline',
+    children: [
+      {
+        title: 'Login',
+        link: '/auth/login',
+      },
+      {
+        title: 'Register',
+        link: '/auth/register',
+      },
+      {
+        title: 'Request Password',
+        link: '/auth/request-password',
+      },
+      {
+        title: 'Reset Password',
+        link: '/auth/reset-password',
+      },
+    ],
+  },
+];
