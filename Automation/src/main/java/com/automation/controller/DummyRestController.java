fcd2bcbe061853b7ecd7b8bbf089407c4c1122e4
Antonio Cecere <antocecere77@gmail.com>
2019-10-05 09:26:48 +0200

    Dummy Automation project

---
+++ Automation/src/main/java/com/automation/controller/DummyRestController.java
@@ -0,0 +1,26 @@
+package com.automation.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins="http://localhost:4200")
+public class DummyRestController {
+
+    @GetMapping(value = "/test")
+    public String getGreetings()
+    {
+        return "Saluti, sono il tuo primo web services";
+    }
+
+    @GetMapping(value = "/saluti/{nome}")
+    public String getSaluti(@PathVariable("nome") String nome)
+    {
+        if(nome.equals("test")) {
+            throw new RuntimeException("Utente disabilitato");
+        }
+        String message = String.format("\"Saluti, %s hai usato il tuo primo web service\"", nome);
+        return message;
+    }
+
+}
